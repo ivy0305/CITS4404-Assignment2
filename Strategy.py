e33efe20679682222f0c2beb7987484ba3286c6c
23270983Ke912 <23270983@student.uwa.edu.au>
2023-04-26 16:44:54 +0800

    add PP BBRSI RSI MACD Strategy

--- Strategy.py
+++ Strategy.py
@@ -38,16 +38,52 @@ class BuyandholdStrategy(Strategy):
         return action
         #done
         
-class PPStrategy(Strategy):
+class BollingerBandRSIStrategy(Strategy):
+    #https://www.youtube.com/watch?v=pCmJ8wsAS_w
     def __init__(self,tradingpercentage):
         self.tradingpercentage=tradingpercentage
         pass
     def decide(self, row):
         action="hold"
-        if(pd.isna(row["previouspivot"]) ):
+        if(pd.isna(row["rsi"]) ):
                 return action
-        if(row["close"]<row["S1"] and row["isuptrend"]):
-            action="sell"
-        if(row["close"]>row["R1"] and not row["isuptrend"]):
+        if(row["low"]<row["lowerband"] and row["rsi"]<30):
             action="buy"
+        if(row["high"]>row["upperband"] and row["rsi"]>70):
+            action="sell"
+        return action
+    
+class RSIStrategy(Strategy):
+    def __init__(self,tradingpercentage):
+        self.tradingpercentage=tradingpercentage
+        pass
+    def decide(self, row):
+        action="hold"
+        if(pd.isna(row["macd_diff"]) ):
+                return action
+        if(row["rsi"]<30):
+            action="buy"
+        if(row["rsi"]>70):
+            action="sell"
+        return action
+    
+class MACDStrategy(Strategy):
+    def __init__(self,tradingpercentage):
+        self.tradingpercentage=tradingpercentage
+        pass
+    def decide(self, row):
+        action="hold"
+        signal=""
+        if(pd.isna(row["macd_diff"]) ):
+                return action
+        if(row["macd"]>row["macd_signal"]):
+            if(signal!="buy" and row["macd"]<0):
+                action="buy"
+            signal="buy"
+        if(row["macd"]<row["macd_signal"]):
+            if(signal!="sell"and row["macd"]>0):
+                action="sell"
+            signal="sell"
+            
+            
         return action
