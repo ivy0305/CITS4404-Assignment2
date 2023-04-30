d660d01726390ecd9b29c5ed33fb5b5f2f7cb3ba
23270983Ke912 <23270983@student.uwa.edu.au>
2023-04-30 23:50:33 +0800

    implement POS

--- Strategy.py
+++ Strategy.py
@@ -1,54 +1,59 @@
 import pandas as pd
 import datetime
+from ta.volatility import BollingerBands, AverageTrueRange
+from ta.trend import MACD,SMAIndicator
+from ta.momentum import RSIIndicator
 class Strategy:
 
     def __init__(self):
         pass
-    def decide(self, row):
+    def decide(self, data,t):
         pass
   
         #done
+'''  
 class PPStrategy(Strategy):
     def __init__(self):
         pass
-    def decide(self, row):
+    def decide(self, data,t):
+        
         action="hold"
-        if(pd.isna(row["previouspivot"]) ):
+        if(pd.isna(data["previouspivot"]) ):
                 return action
 
-        if(row["close"]<row["S2"] and row["isuptrend"]):
+        if(data["close"]<data["S2"] and data["isuptrend"]):
             action="buy"
                 
-        if(row["close"]>row["R2"] and not row["isuptrend"]):
+        if(data["close"]>data["R2"] and not data["isuptrend"]):
             action="sell"      
         return action
         #done
+'''  
 class BuyandholdStrategy(Strategy):
     def __init__(self):
-        self.i=0
-    def decide(self, row):
+        pass
+    def decide(self, data,t):
         action="hold"
-        if(self.i==0):
+        if(t==1):
             action="buy"
-        self.i+=1
-
         return action
         #done
-        
+    '''
+     
 class BollingerBandRSIStrategy(Strategy):
     #https://www.youtube.com/watch?v=pCmJ8wsAS_w
     def __init__(self):
         pass
-    def decide(self, row):
+    def decide(self, data,t):
         action="hold"
         #skip if no rsi data
-        if(pd.isna(row["rsi"]) ):
+        if(pd.isna(data["rsi"]) ):
                 return action
     
-        if(row["close"]<row["lowerband"] and row["rsi"]<=30):
+        if(data["close"]<data["lowerband"] and data["rsi"]<=30):
             action="buy"
     
-        if(row["high"]>row["moving_average"]):
+        if(data["high"]>data["moving_average"]):
             action="sell"
        
         #print("Date:",datetime.datetime.fromtimestamp(row["timestamp"]/1000.0),row["close"] ,action)
@@ -59,20 +64,20 @@ class RSIStrategy(Strategy):
   
         self.signal="hold"
         pass
-    def decide(self, row):
+    def decide(self, data,t):
         action="hold"
-        if(pd.isna(row["macd_diff"]) ):
+        if(pd.isna(data["macd_diff"]) ):
                 return action
      
-        if(self.signal=="buy" and row["rsi"]>30):
+        if(self.signal=="buy" and data["rsi"]>30):
             self.signal="hold"
             action="buy"  
-        if(self.signal=="sell" and row["rsi"]<70):
+        if(self.signal=="sell" and data["rsi"]<70):
             self.signal="hold"
             action="sell"
-        if(row["rsi"]>70):
+        if(data["rsi"]>70):
             self.signal="sell"
-        if(row["rsi"]<30):
+        if(data["rsi"]<30):
             self.signal="buy"
         return action
     
@@ -81,15 +86,15 @@ class MACDStrategy(Strategy):
     def __init__(self):
         self.signal="hold"
         pass
-    def decide(self, row):
+    def decide(self, data,t):
         action="hold"
-        if(pd.isna(row["macd_diff"]) ):
+        if(pd.isna(data["macd_diff"]) ):
                 return action
-        if(self.signal=="buy" and row["macd"]>row["macd_signal"] and row["macd"]< 0):
+        if(self.signal=="buy" and data["macd"]>data["macd_signal"] and data["macd"]< 0):
             action="buy"
             self.signal="hold"
     
-        if(self.signal=="sell" and row["macd"]<row["macd_signal"] and row["macd"]>0 ):
+        if(self.signal=="sell" and data["macd"]<row["macd_signal"] and row["macd"]>0 ):
             action="sell"
             self.signal="hold"
             
@@ -98,34 +103,65 @@ class MACDStrategy(Strategy):
         else:
             self.signal="sell"
         return action
+         '''  
 class MACDRSIStrategy(Strategy):
     #https://www.youtube.com/watch?v=rf_EQvubKlk
-    def __init__(self):
+    def __init__(self,slow,fast,sign,rsiwindow,buythreshold,sellthreshold,macdthreshold):
         self.signal="hold"
+        self.slow=slow
+        self.fast=fast
+        self.sign=sign
+        self.rsiwindow=rsiwindow
+        self.buythreshold=buythreshold
+        self.sellthreshold=sellthreshold
+        self.minimumworkingday=slow+sign-2 
+        self.macdthreshold=macdthreshold
+        self.close=pd.DataFrame(columns=["close"])
+        
         pass
-    def decide(self, row):
+    def decide(self, data,t):
         action="hold"
-        if(pd.isna(row["macd_diff"]) ):
-                return action
-        if(self.signal=="buy" and row["macd"]>row["macd_signal"] and row["macd"]< 0 and row["rsi"]<50):
+        if(t<=self.minimumworkingday):
+            return action
+        macd_indicator=MACD(data["close"],window_slow=self.slow,window_fast=self.fast,window_sign=self.sign)
+        macd_signal=macd_indicator.macd_signal()[t-1]
+        macd=macd_indicator.macd()[t-1]
+        #macd_diff=macd_indicator.macd_diff()[-1]
+  
+        RSI_indicator=RSIIndicator(data["close"],window=self.rsiwindow)
+        rsi=RSI_indicator.rsi()[t-1]
+        if(macd>macd_signal and macd< self.macdthreshold and rsi<self.buythreshold):
+            action="buy"
+        if( macd<macd_signal and macd>self.macdthreshold and rsi>self.sellthreshold):
+            action="sell"
+          
+        '''
+         if(self.signal=="buy" and macd>macd_signal and macd< self.macdthreshold and rsi<self.buythreshold):
             action="buy"
             self.signal="hold"
     
-        if(self.signal=="sell" and row["macd"]<row["macd_signal"] and row["macd"]>0  and row["rsi"]>50):
+        if(self.signal=="sell" and macd<macd_signal and macd>self.macdthreshold and rsi>self.sellthreshold):
             action="sell"
             self.signal="hold"
             
-        if(row["macd"]<row["macd_signal"]):
+        if(macd<macd_signal):
             self.signal="buy"
         else:
             self.signal="sell"
+        
+        '''    
+       
         return action
+    
+'''  
+    
 class VotingStrategy(Strategy):
     def __init__(self,strategyList):
         self.StrategyList=strategyList
-    def decide(self, row):
+    def decide(self, data,t):
         decisionlist=[]
         for strategy in self.StrategyList:
-           decisionlist.append(strategy.decide(row))
+           decisionlist.append(strategy.decide(data))
         #print(decisionlist)
         return max(set(decisionlist), key = decisionlist.count)
+'''  
--- run_test.py
+++ run_test.py
@@ -1,4 +1,5 @@
 import ccxt
+from Problem import *
 from Bot import *
 from Strategy import *
 import pandas as pd
@@ -26,60 +27,79 @@ def addIndicators(df):
     df["macd_signal"]=macd_indicator.macd_signal()
     df["macd"]=macd_indicator.macd()
     df["macd_diff"]=macd_indicator.macd_diff()
-
     
     RSI_indicator=RSIIndicator(df["close"],12)
     df["rsi"]=RSI_indicator.rsi()
 
-    df['previouspivot'] = (df['high'] + df['low'] + df['close'])/3
-    df['R1'] = (2*df['previouspivot']) - df['low']
-    df['S1'] = (2*df['previouspivot']) - df['high']
-    df['R2'] = (df['previouspivot']) + (df['high'] - df['low'])
-    df['S2'] = (df['previouspivot']) - (df['high'] - df['low'])
-    df['previouspivot']=df['previouspivot'].shift(1)
-    df['R1']=df['R1'].shift(1)
-    df['S1']=df['S1'].shift(1)
-    df['R2']=df['R2'].shift(1)
-    df['S2']=df['S2'].shift(1)
-    df["isuptrend"]=df["open"]>df["previouspivot"]
     return df 
 # Retrieve the historical data
 DATANUMBER=720
 kraken=ccxt.kraken()
 kraken.load_markets()
 
-ohlcdata=kraken.fetch_ohlcv("BTC/AUD",since=1620403200000,timeframe="4h",limit=DATANUMBER) #Since 1620403200000 8 May 2021 (+ 720days = 28 Apr 2023)
+ohlcdata=kraken.fetch_ohlcv("BTC/AUD",since=1620403200000,timeframe="1d",limit=DATANUMBER) #Since 1620403200000 8 May 2021 (+ 720days = 28 Apr 2023)
 
 df=pd.DataFrame(ohlcdata,columns=["timestamp","open","high","low","close","volume"])
 
-df=addIndicators(df)
+#df=addIndicators(df)
 print(df)
 # *** Simulations *** #
 
+Problem=BTCProblem(df)
+algorithm = PSO(adaptive=True, pop_size=100, sampling=IntegerRandomSampling(), repair=RoundingRepair(), pertube_best=False)
 
-# Simulates a defined number of days of trade simulations with given parameters and starting cash.
+res = minimize(Problem,
+               algorithm,
+               seed=1,
+               verbose=True)
+print("Best solution found: \nX = %s\nF = $%s\nCV = %s\nG=%s\n" % (res.X, -1 * res.F[0], res.CV[0],res.G[0]))
+
+
+
 startingbtc=0
 startingaud=100
+MACDRSIstrategy=MACDRSIStrategy(slow=res.X[0],fast=res.X[1],sign=res.X[2],rsiwindow=res.X[3],buythreshold=res.X[4],sellthreshold=res.X[5],macdthreshold=res.X[6])
+MACDRSIBot=Bot("PSO MACD RSI Strategy",MACDRSIstrategy, startingbtc, startingaud)
 
 BHStrategy= BuyandholdStrategy()
-PPstrategy= PPStrategy()
-BRSIstrategy= BollingerBandRSIStrategy()
-RSIstrategy= RSIStrategy()
-MACDstrategy= MACDStrategy()
-MACDRSIstrategy=MACDRSIStrategy()
-Votestrategy= VotingStrategy([MACDstrategy,RSIstrategy,PPstrategy])
 BHBot=Bot("Buy and hold Strategy",BHStrategy, startingbtc, startingaud)
-PPBot=Bot("PP Support and Resistance Strategy",PPstrategy, startingbtc, startingaud)
-BRSIBot=Bot("BollingerBand RSI Strategy",BRSIstrategy, startingbtc, startingaud)
-RSIBot=Bot("RSI Strategy",RSIstrategy, startingbtc, startingaud)
-VoteBot=Bot("Voting Strategy",Votestrategy, startingbtc, startingaud)
-MACDBot=Bot("MACD Strategy",MACDstrategy, startingbtc, startingaud)
-MACDRSIstrategy=Bot("MACD RSI Strategy",MACDRSIstrategy, startingbtc, startingaud)
-TestBots=[BHBot,PPBot,RSIBot,BRSIBot,MACDBot,MACDRSIstrategy,VoteBot]
+TestBots=[BHBot,MACDRSIBot]
 for bot in TestBots:
     print("*"*50,f'{bot.getname():^30}',"*"*50)
     bot.execute_trade(df) 
   
     print("Result:",round(bot.score(),3),"%")
     print("Total Trade Made:",len(bot.gettradingrecord())," times")
-    #printrecord(bot)
+    printrecord(bot)
+
+
+'''
+# Simulates a defined number of days of trade simulations with given parameters and starting cash.
+startingbtc=0
+startingaud=100
+
+BHStrategy= BuyandholdStrategy()
+#PPstrategy= PPStrategy()
+#BRSIstrategy= BollingerBandRSIStrategy()
+#RSIstrategy= RSIStrategy()
+#MACDstrategy= MACDStrategy()
+MACDRSIstrategy=MACDRSIStrategy(slow=26,fast=12,sign=8,rsiwindow=14,buythreshold=40,sellthreshold=60)
+#Votestrategy= VotingStrategy([MACDstrategy,RSIstrategy,PPstrategy])
+BHBot=Bot("Buy and hold Strategy",BHStrategy, startingbtc, startingaud)
+#PPBot=Bot("PP Support and Resistance Strategy",PPstrategy, startingbtc, startingaud)
+#BRSIBot=Bot("BollingerBand RSI Strategy",BRSIstrategy, startingbtc, startingaud)
+#RSIBot=Bot("RSI Strategy",RSIstrategy, startingbtc, startingaud)
+#VoteBot=Bot("Voting Strategy",Votestrategy, startingbtc, startingaud)
+#MACDBot=Bot("MACD Strategy",MACDstrategy, startingbtc, startingaud)
+MACDRSIBot=Bot("MACD RSI Strategy",MACDRSIstrategy, startingbtc, startingaud)
+TestBots=[BHBot,MACDRSIBot]
+for bot in TestBots:
+    print("*"*50,f'{bot.getname():^30}',"*"*50)
+    bot.execute_trade(df) 
+  
+    print("Result:",round(bot.score(),3),"%")
+    print("Total Trade Made:",len(bot.gettradingrecord())," times")
+    printrecord(bot)
+
+
+'''
